2d5991e68aef65c62215500a9abcbba63e1ba8f8
Mahin556 <mahinraza556@gmail.com>
2025-12-27 14:58:50 +0530

    update the helm installation

--- jenkins.sh
+++ jenkins.sh
@@ -82,7 +82,11 @@ sudo apt-get update
 sudo apt-get install trivy -y
 
 # Intalling Helm
-sudo snap install helm --classic
+sudo apt-get install gpg apt-transport-https --yes
+curl -fsSL https://packages.buildkite.com/helm-linux/helm-debian/gpgkey | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/helm.gpg] https://packages.buildkite.com/helm-linux/helm-debian/any/ any main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
 
 #Verify
 jenkins --version
