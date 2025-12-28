92060a788c560fc21b2dd32023b3dd952c8098fc
Mahin556 <mahinraza556@gmail.com>
2025-12-29 00:54:13 +0530

    Update the scripts

--- jenkins.sh
+++ jenkins.sh
@@ -8,9 +8,9 @@ export DEBIAN_FRONTEND=noninteractive
 apt update -y
 
 # Install Java 17 (Jenkins supported)
-apt install -y fontconfig openjdk-17-jdk
+apt install -y fontconfig openjdk-17-jdk openjdk-17-jdk
 
-# Jenkins install
+# Config Jenkins 
 wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
   
@@ -18,19 +18,12 @@ echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
 https://pkg.jenkins.io/debian-stable binary/" \
 > /etc/apt/sources.list.d/jenkins.list
 
-apt update -y
-apt install -y jenkins
-systemctl enable --now jenkins
 
-# Installing Docker 
+# Config Docker 
 apt install ca-certificates curl -y
-
 install -m 0755 -d /etc/apt/keyrings
-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-
 chmod a+r /etc/apt/keyrings/docker.asc
-
 # Add the repository to Apt sources:
 tee /etc/apt/sources.list.d/docker.sources <<EOF
 Types: deb
@@ -40,12 +33,6 @@ Components: stable
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
-apt update -y
-apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-systemctl enable --now docker
-usermod -aG docker ubuntu
-usermod -aG docker jenkins
-systemctl restart docker
 
 
 # If you don't want to install Jenkins, you can create a container of Jenkins
@@ -60,16 +47,12 @@ curl -L "https://github.com/docker/compose/releases/download/v2.29.2/docker-comp
 chmod +x /usr/local/bin/docker-compose
 
 # Installing AWS CLI
-#!/bin/bash
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 apt install unzip -y
-nzip awscliv2.zip
+unzip awscliv2.zip
 ./aws/install
 
 # Installing Kubectl
-#!/bin/bash
-apt update
-apt install curl -y
 curl -LO "https://dl.k8s.io/release/v1.28.4/bin/linux/amd64/kubectl"
 chmod +x kubectl
 mv kubectl /usr/local/bin/
@@ -80,25 +63,42 @@ curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/d
 mv /tmp/eksctl /usr/local/bin
 eksctl version
 
-# Installing Terraform
+# Config Terraform
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-apt update
-apt install terraform -y
 
-# Installing Trivy
+# Config Trivy
 apt-get install wget apt-transport-https gnupg lsb-release -y
 wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
 echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb generic main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
-apt-get update
-apt-get install trivy -y
 
-# Intalling Helm
+# Config Helm
 apt-get install gpg apt-transport-https --yes
 curl -fsSL https://packages.buildkite.com/helm-linux/helm-debian/gpgkey | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
 echo "deb [signed-by=/usr/share/keyrings/helm.gpg] https://packages.buildkite.com/helm-linux/helm-debian/any/ any main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
-apt-get update
-apt-get install helm
+
+#Update Repo List
+apt update -y
+
+#Install jenkins
+apt install -y jenkins
+systemctl enable --now jenkins
+
+#Install Docker
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+systemctl enable --now docker
+usermod -aG docker ubuntu
+usermod -aG docker jenkins
+systemctl restart docker
+
+#Install Terraform
+apt install terraform -y
+
+#Install Trivy
+apt install trivy -y
+
+#Install Helm
+apt-get install helm -y
 
 #Verify
 jenkins --version
